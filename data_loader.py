da7c99ef8485039108fff1de2fa80943ec809869
conceptacid <vasiliy.shmyrev@gmail.com>
2017-10-01 06:48:58 -0700

    session 3

--- data_loader.py
+++ data_loader.py
@@ -12,71 +12,141 @@ dev_data_triplets_filename = 'data/Freebase/dev.txt'
 d=100
 K=4
 lambd = 0.0001
+C = 2
 
 def read_ids(filename):
-	res = {}
-	counter = 0
-	with open(filename, 'r') as file:
-		reader = csv.reader(file, delimiter='\t')
-		for row in reader:
-			res[row[0]] = counter
-			counter +=1 
-	return res
+    res = {}
+    counter = 0
+    with open(filename, 'r') as file:
+        reader = csv.reader(file, delimiter='\t')
+        for row in reader:
+            res[row[0]] = counter
+            counter +=1
+    return res
 
-def read_triplets(filename, entity_lookup={}, relation_lookup={}, read_groud_truth=False):
-	res = []
-	with open(filename, 'r') as file:
-		reader = csv.reader(file, delimiter='\t')
-		for row in reader:
-			if read_groud_truth:
-				e1, r, e2, ground_truth = row
-				res.append((entity_lookup[e1], relation_lookup[r], entity_lookup[e2], int(ground_truth)))
-			else:
-				e1, r, e2 = row
-				res.append((entity_lookup[e1], relation_lookup[r], entity_lookup[e2]))
-	return res
+def read_tuples(filename, entity_lookup={}, relation_lookup={}, read_groud_truth=False):
+    res = []
+    with open(filename, 'r') as file:
+        reader = csv.reader(file, delimiter='\t')
+        for row in reader:
+            if read_groud_truth:
+                e1, r, e2, ground_truth = row
+                res.append((entity_lookup[e1], relation_lookup[r], entity_lookup[e2], int(ground_truth)))
+            else:
+                e1, r, e2 = row
+                res.append((entity_lookup[e1], relation_lookup[r], entity_lookup[e2]))
+    return res
 
 def add_corrupted_exampes(train_data, C, entity_size):
-	result = []
-	for triplet in train_data:
-		for c in range(C):
-			quad = triplet + (random.randint(0, entity_size-1),)
-			result.append(quad)
-	return result
+    result = []
+    for triplet in train_data:
+        for c in range(C):
+            quad = triplet + (random.randint(0, entity_size-1),)
+            result.append(quad)
+    return result
+
 
 def define_parameters(d=d, Ne=None, Nr=None, K=K):
-	params = dict(
-		U=tf.get_variable(shape=(K,1), name="U",   initializer=tf.contrib.layers.xavier_initializer(), regularizer=tf.contrib.layers.l2_regularizer(scale=lambd)),
-		V=tf.get_variable(shape=(K,2*d), name="V", initializer=tf.contrib.layers.xavier_initializer(), regularizer=tf.contrib.layers.l2_regularizer(scale=lambd)),
-		b=tf.get_variable(shape=(K,1), name="b",   initializer=tf.zeros_initializer(), regularizer=tf.contrib.layers.l2_regularizer(scale=lambd)),
-		E=tf.get_variable(shape=(d,Ne), name="E",  initializer=tf.contrib.layers.xavier_initializer(), regularizer=tf.contrib.layers.l2_regularizer(scale=lambd))
-	)
-	for r in range(Nr):
-		for k in range(K):
-			name = "W_"+ str(r) + "_"+str(k)
-			params[name] = tf.get_variable(shape=(d,d), name=name, initializer=tf.contrib.layers.xavier_initializer(), regularizer=tf.contrib.layers.l2_regularizer(scale=lambd))
-	return params
+    params = dict(
+        E=tf.get_variable(shape=(d,Ne), name="E",  initializer=tf.contrib.layers.xavier_initializer(), regularizer=tf.contrib.layers.l2_regularizer(scale=lambd))
+    )
 
-def define_graph(params, batch_size):
-	X=tf.placeholder(dtype=tf.float64, shape=(4,batch_size))
-
-	e1_indices = tf.slice(X, begin=(0,0), size=(1,batch_size) )
-	e1 = tf.gather( params["E"], e1_indices, axis=1 )
-
-	r_indices = tf.slice(X, begin=(1,0), size=(1,batch_size) )
-
-	e2_indices = tf.slice(X, begin=(2,0), size=(1,batch_size) )
-	e2 = tf.gather( params["E"], e2_indices, axis=1 )
-
-	c_indices = tf.slice(X, begin=(3,0), size=(1,batch_size) )
-	c = tf.gather( params["E"], c_indices, axis=1 )
+    for r in range(Nr):
+        U=tf.get_variable(shape=(K,1), name="U_" + str(r),   initializer=tf.contrib.layers.xavier_initializer(),
+                          regularizer=tf.contrib.layers.l2_regularizer(scale=lambd))
+        V=tf.get_variable(shape=(K,2*d), name="V_" + str(r), initializer=tf.contrib.layers.xavier_initializer(),
+                          regularizer=tf.contrib.layers.l2_regularizer(scale=lambd))
+        b=tf.get_variable(shape=(K,1), name="b_" + str(r),   initializer=tf.zeros_initializer(),
+                          regularizer=tf.contrib.layers.l2_regularizer(scale=lambd))
+        params["U_" + str(r)] = U
+        params["V_" + str(r)] = V
+        params["b_" + str(r)] = b
+        for k in range(K):
+            name = "W_"+ str(r) + "_"+str(k)
+            params[name] = tf.get_variable(shape=(d,d), name=name, initializer=tf.contrib.layers.xavier_initializer(), regularizer=tf.contrib.layers.l2_regularizer(scale=lambd))
+    return params
 
 
-	#for k in range(K):
-		#Wr = params["W_" + str(r)] ...
+def build_g(params, E1, E2, r):
+    SlicesArray = []
+    for k in range(K):
+        Wrk = params["W_" + str(r) + "_" + str(k)]
 
-	return X, g, cost
+        #print("Wrk.shape=", Wrk.shape)
+        #print("E2.shape=", E2.shape)
 
+        Temp = tf.multiply(E1, tf.matmul(Wrk, E2))
+        Slice = tf.reduce_sum(Temp, axis=0, keep_dims=True)
+
+        #print("Slice.shape = ", Slice.shape)
+
+        SlicesArray.append(Slice)
+    eWe = tf.concat(SlicesArray, axis=0)
+
+    # Vr should have shape (K,2*d)
+    Vr = params["V_" + str(r)]
+    assert(Vr.shape == (K,2*d))
+
+    # br should have shape (K,1)
+    br = params["b_" + str(r)]
+    assert(br.shape == (K,1))
+
+    #print("EWE.shape =", eWe.shape)
+
+    Activation = tf.tanh(eWe + tf.matmul(Vr, tf.concat([E1, E2], axis=0)) + br) 
+
+    g = tf.matmul(tf.transpose(params["U_" + str(r)]), Activation)
+    assert(g.shape[0] == 1)
+    return g
+
+
+def define_graph(params, Nr, K):
+
+    Xs = {}
+    sums_r = []
+
+    for r in range(Nr):
+        X = tf.placeholder(dtype=tf.int64, shape=(4,None), name="X_" + str(r))
+
+        Xs["X_" + str(r)] = X
+
+        E1_indices = tf.slice(X, begin=(0,0), size=(1,-1), name="E1_index_" + str(r))
+        #print("E1_indices.shape=", E1_indices.shape)
+        E1 = tf.gather( params["E"], E1_indices, axis=1, name="E1_" + str(r) )
+        E1 = tf.reshape(E1,(d,-1))     # todo: verify
+        #E1 shape should be (d,m_r)
+
+        #print("E1.shape=", E1.shape)
+
+        E2_indices = tf.slice(X, begin=(2,0), size=(1,-1), name="E2_index_" + str(r) )
+        E2 = tf.gather( params["E"], E2_indices, axis=1, name="E2_" + str(r) )
+        E2 = tf.reshape(E2, (d, -1))
+
+        C_indices = tf.slice(X, begin=(3,0), size=(1,-1), name="C_index_" + str(r) )
+        C = tf.gather( params["E"], C_indices, axis=1, name="C" + str(r)  )
+        C = tf.reshape(C, (d, -1))
+
+        g = build_g(params, E1, E2, r)
+
+        gc = build_g(params, E1, C, r)
+
+        difference = tf.constant(1.) - g + gc
+        max_term = tf.maximum(tf.constant(0.), difference)
+        sum_r = tf.reduce_sum(max_term)
+        sums_r.append(sum_r)
+
+    cost = tf.add_n(sums_r)
+    return Xs, cost
+
+
+def create_data_feed(Xs, quadruplets, Nr):
+    res = {}
+    for r in range(Nr):
+        name = "X_" + str(r)
+        res[Xs[name]] = quadruplets[:, quadruplets[1, :] == r]
+        print("r=",r, "\n", res[Xs[name]] )
+        #print("r=",r, "\n", quadruplets[1, :] == r)
+    return res
 
 
 entity_lookup = read_ids(entities_filename)
@@ -84,13 +154,24 @@ relation_lookup = read_ids(relations_filename)
 
 Ne = len(entity_lookup)
 Nr = len(relation_lookup)
+print("Number of entities = ", Ne)
+print("Number of relations = ", Nr)
 
-train_data = read_triplets(train_data_triplets_filename, entity_lookup=entity_lookup, relation_lookup=relation_lookup)
-test_data = read_triplets(test_data_triplets_filename, entity_lookup=entity_lookup, relation_lookup=relation_lookup, read_groud_truth=True)
-dev_data = read_triplets(dev_data_triplets_filename, entity_lookup=entity_lookup, relation_lookup=relation_lookup, read_groud_truth=True)
+train_data_tuples = read_tuples(train_data_triplets_filename, entity_lookup=entity_lookup, relation_lookup=relation_lookup)
+test_data_tuples = read_tuples(test_data_triplets_filename, entity_lookup=entity_lookup, relation_lookup=relation_lookup, read_groud_truth=True)
+dev_data_tuples = read_tuples(dev_data_triplets_filename, entity_lookup=entity_lookup, relation_lookup=relation_lookup, read_groud_truth=True)
 
+train_data = np.array(add_corrupted_exampes(train_data_tuples, C, Ne)).T
 
 #print(add_corrupted_exampes(train_data, 2, len(entity_lookup)))
-p = define_parameters(Ne=Ne, Nr=len(relation_lookup))
-print(p)
+params = define_parameters(Ne=Ne, Nr=len(relation_lookup))
+Xs, cost = define_graph(params, Nr, K)
 
+data_feed = create_data_feed(Xs, train_data, Nr)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    cost_value = sess.run(cost, feed_dict=data_feed)
+    print(cost_value)
